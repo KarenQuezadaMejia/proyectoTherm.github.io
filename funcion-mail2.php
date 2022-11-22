b01c4bb7a72528dedf89e8710d7f24a5e708b41e
karenQ <karen.quezada@toctoc.com>
2022-11-21 23:04:36 -0300

    fix files

--- funcion-mail2.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$sub = "Tu tema";
-//el mensaje 
-$msg = "Tu mensaje";
-//correo electrónico del destinatario aquí
-$rec = "jmquezadam95@gmail.com";
-//enviar correo electrónico
-mail ($rec,$sub,$msg);
-?>
